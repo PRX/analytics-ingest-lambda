a4378b54f71ad6f3e0a33eeed5865eda5e578495
cavis <ryancavis@gmail.com>
2017-08-11 12:17:00 -0600

    Look up agent ids

--- test/handler-test.js
+++ test/handler-test.js
@@ -63,7 +63,7 @@ describe('handler', () => {
       expect(downloadJson.path).to.equal('the/path/here');
       expect(downloadJson.feeder_podcast).to.equal(1234);
       expect(downloadJson.feeder_episode).to.equal('1234-5678');
-      expect(downloadJson.remote_agent).to.equal('some agent string');
+      expect(downloadJson.remote_agent).to.equal('AppleCoreMedia/1.0.0.14B100 (iPhone; U; CPU OS 10_1_1 like Mac OS X; en_us)');
       expect(downloadJson.remote_ip).to.equal('24.49.134.194, 127.0.0.1, 99.99.99.99, whatev');
       expect(downloadJson.timestamp).to.equal(1487703699);
       expect(downloadJson.request_uuid).to.equal('req-uuid');
@@ -72,6 +72,9 @@ describe('handler', () => {
       expect(downloadJson.cause).to.be.null;
       expect(downloadJson.city_id).to.equal(5576882);
       expect(downloadJson.country_id).to.equal(6252001);
+      expect(downloadJson.agent_name_id).to.equal(25);
+      expect(downloadJson.agent_type_id).to.equal(36);
+      expect(downloadJson.agent_os_id).to.equal(43);
 
       expect(inserted['the_impressions_table$20170221'].length).to.equal(2);
       expect(inserted['the_impressions_table$20170221'][0].insertId).not.to.equal('req-uuid');
@@ -82,7 +85,7 @@ describe('handler', () => {
       expect(impressionJson.path).to.equal('the/path/here');
       expect(impressionJson.feeder_podcast).to.equal(1234);
       expect(impressionJson.feeder_episode).to.equal('1234-5678');
-      expect(impressionJson.remote_agent).to.equal('some agent string');
+      expect(impressionJson.remote_agent).to.equal('curl/7.35.0');
       expect(impressionJson.remote_ip).to.equal('151.101.129.67');
       expect(impressionJson.timestamp).to.equal(1487703699);
       expect(impressionJson.request_uuid).to.equal('req-uuid');
@@ -94,6 +97,9 @@ describe('handler', () => {
       expect(impressionJson.cause).to.be.null;
       expect(impressionJson.city_id).to.equal(5391959);
       expect(impressionJson.country_id).to.equal(6252001);
+      expect(impressionJson.agent_name_id).to.be.null;
+      expect(impressionJson.agent_type_id).to.be.null;
+      expect(impressionJson.agent_os_id).to.be.null;
 
       impressionJson = inserted['the_impressions_table$20170221'][1].json;
       expect(impressionJson.ad_id).to.equal(98);
@@ -101,6 +107,9 @@ describe('handler', () => {
       expect(impressionJson.cause).to.equal('something');
       expect(impressionJson.city_id).to.equal(4407066);
       expect(impressionJson.country_id).to.equal(6252001);
+      expect(impressionJson.agent_name_id).to.be.null;
+      expect(impressionJson.agent_type_id).to.be.null;
+      expect(impressionJson.agent_os_id).to.be.null;
 
       impressionJson = inserted['the_impressions_table$20170222'][0].json;
       expect(impressionJson.ad_id).to.equal(76);
@@ -108,6 +117,9 @@ describe('handler', () => {
       expect(impressionJson.cause).to.equal(null);
       expect(impressionJson.city_id).to.be.null;
       expect(impressionJson.country_id).to.be.null;
+      expect(impressionJson.agent_name_id).to.equal(21);
+      expect(impressionJson.agent_type_id).to.equal(36);
+      expect(impressionJson.agent_os_id).to.equal(42);
 
       done();
     });
