1bea14c67cd9b3feddc21d8a631407cfbbf6e778
cavis <ryancavis@gmail.com>
2018-12-12 10:17:52 -0700

    Move legacy record types into separate files

---
+++ test/legacy-inputs-dovetail-impressions-test.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const support = require('./support');
+const bigquery = require('../lib/bigquery');
+const LegacyDovetailImpressions = require('../lib/inputs/legacy-dovetail-impressions');
+
+describe('legacy-dovetail-impressions', () => {
+
+  let impression = new LegacyDovetailImpressions();
+
+  it('recognizes impression records', () => {
+    expect(impression.check({type: null})).to.be.false;
+    expect(impression.check({type: undefined})).to.be.false;
+    expect(impression.check({type: undefined, adId: 1})).to.be.true;
+    expect(impression.check({type: 'download'})).to.be.false;
+    expect(impression.check({type: 'impression'})).to.be.true;
+  });
+
+  it('formats table inserts', () => {
+    return impression.format({requestUuid: 'the-uuid', timestamp: 1490827132999}).then(row => {
+      expect(row).to.have.keys('table', 'record');
+      expect(row.table).to.equal('dt_impressions');
+      expect(row.record).to.have.keys('insertId', 'json');
+      expect(row.record.insertId).not.to.equal('the-uuid');
+      expect(row.record.json).to.have.keys(
+        'timestamp',
+        'request_uuid',
+        'feeder_podcast',
+        'feeder_episode',
+        'is_duplicate',
+        'cause',
+        'ad_id',
+        'campaign_id',
+        'creative_id',
+        'flight_id'
+      );
+      expect(row.record.json.timestamp).to.equal(1490827132);
+      expect(row.record.json.request_uuid).to.equal('the-uuid');
+    });
+  });
+
+  it('creates unique insert ids for ads', () => {
+    let formats = [
+      impression.format({requestUuid: 'req1', adId: 1}),
+      impression.format({requestUuid: 'req1', adId: 1}),
+      impression.format({requestUuid: 'req1', adId: 2}),
+      impression.format({requestUuid: 'req2', adId: 1})
+    ];
+    return Promise.all(formats).then(datas => {
+      expect(datas[0].record.insertId).to.equal(datas[1].record.insertId);
+      expect(datas[0].record.insertId).not.to.equal(datas[2].record.insertId);
+      expect(datas[0].record.insertId).not.to.equal(datas[3].record.insertId);
+    });
+  });
+
+  it('inserts nothing', () => {
+    return impression.insert().then(result => {
+      expect(result.length).to.equal(0);
+    });
+  });
+
+  it('inserts impression records', () => {
+    let inserts = {};
+    sinon.stub(bigquery, 'insert', (tbl, rows) => {
+      inserts[tbl] = rows;
+      return Promise.resolve(rows.length);
+    });
+    let impression2 = new LegacyDovetailImpressions([
+      {type: 'impression', requestUuid: 'the-uuid1', timestamp: 1490827132999},
+      {type: 'download', requestUuid: 'the-uuid2', timestamp: 1490827132999},
+      {type: 'impression', requestUuid: 'the-uuid3', timestamp: 1490837132},
+      {type: 'impression', requestUuid: 'the-uuid4', timestamp: 1490827132999},
+    ]);
+    return impression2.insert().then(result => {
+      expect(result.length).to.equal(1);
+      expect(result[0].dest).to.equal('dt_impressions');
+      expect(result[0].count).to.equal(3);
+      expect(inserts['dt_impressions'][0].json.request_uuid).to.equal('the-uuid1');
+      expect(inserts['dt_impressions'][1].json.request_uuid).to.equal('the-uuid3');
+      expect(inserts['dt_impressions'][2].json.request_uuid).to.equal('the-uuid4');
+    });
+  });
+
+});
---
+++ test/legacy-inputs-redis-increments-test.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const support = require('./support');
+const redis = require('../lib/redis');
+const LegacyRedisIncrements = require('../lib/inputs/legacy-redis-increments');
+
+describe('legacy-redis-increments', () => {
+
+  beforeEach(() => process.env.REDIS_HOST = 'redis://127.0.0.1:6379');
+
+  it('recognizes download records', () => {
+    let incr = new LegacyRedisIncrements();
+    expect(incr.check({})).to.be.false;
+    expect(incr.check({type: 'foobar', feederPodcast: 1})).to.be.false;
+    expect(incr.check({type: 'download', feederPodcast: 1})).to.be.true;
+    expect(incr.check({type: 'impression', feederPodcast: 1})).to.be.false;
+    expect(incr.check({type: 'download', feederPodcast: 1, isDuplicate: true})).to.be.false;
+    expect(incr.check({type: 'download', feederPodcast: 1, isDuplicate: false})).to.be.true;
+  });
+
+  it('inserts nothing for no records', () => {
+    let incr = new LegacyRedisIncrements();
+    return incr.insert().then(result => {
+      expect(result.length).to.equal(0);
+    });
+  });
+
+  it('inserts nothing for records without feeder ids/guids', () => {
+    let incr = new LegacyRedisIncrements([
+      record(1490827132, 'download', null, null),
+      record(1490827132, 'impression', null, null)
+    ]);
+    return incr.insert().then(result => {
+      expect(result.length).to.equal(0);
+    });
+  });
+
+  it('inserts nothing for no redis host', () => {
+    process.env.REDIS_HOST = '';
+    let records = [record(1490827132, 'download', 1234, 'abcd')];
+    let incr = new LegacyRedisIncrements(records);
+    return incr.insert().then(result => {
+      expect(result.length).to.equal(0);
+
+      process.env.REDIS_HOST = 'whatev';
+      incr = new LegacyRedisIncrements(records);
+      return incr.insert();
+    }).then(result => {
+      expect(result.length).to.equal(1);
+    });
+  });
+
+  it('increments redis counts', () => {
+    let incr = new LegacyRedisIncrements([
+      record(1490827132, 'download', 1234, 'abcd'),
+      record(1490827132, 'download', 1234, 'efgh'),
+      record(1490828132, 'download', 1234, 'abcd'),
+      record(1490829132, 'download', 1234, 'abcd'),
+      record(1490827132, 'impression', 1234, null),
+      record(1490827132, 'impression', 1234, 'efgh', true)
+    ]);
+    return incr.insert().then(result => {
+      expect(result.length).to.equal(1);
+      expect(result[0].dest).to.equal('redis://127.0.0.1');
+      expect(result[0].count).to.equal(4 * 4); // skips impressions
+      return support.redisGetAll('downloads.podcasts.*');
+    }).then(vals => {
+      expect(Object.keys(vals).length).to.equal(3);
+      expect(vals['downloads.podcasts.HOUR.2017-03-29T22:00:00Z']['1234']).to.equal('3');
+      expect(vals['downloads.podcasts.HOUR.2017-03-29T23:00:00Z']['1234']).to.equal('1');
+      expect(vals['downloads.podcasts.DAY.2017-03-29T00:00:00Z']['1234']).to.equal('4');
+      return support.redisGetAll('impressions.podcasts.*');
+    }).then(vals => {
+      expect(Object.keys(vals).length).to.equal(0);
+      return support.redisGetAll('downloads.episodes.*');
+    }).then(vals => {
+      expect(Object.keys(vals).length).to.equal(3);
+      expect(vals['downloads.episodes.HOUR.2017-03-29T22:00:00Z']['abcd']).to.equal('2');
+      expect(vals['downloads.episodes.HOUR.2017-03-29T22:00:00Z']['efgh']).to.equal('1');
+      expect(vals['downloads.episodes.HOUR.2017-03-29T23:00:00Z']['abcd']).to.equal('1');
+      expect(vals['downloads.episodes.DAY.2017-03-29T00:00:00Z']['abcd']).to.equal('3');
+      expect(vals['downloads.episodes.DAY.2017-03-29T00:00:00Z']['efgh']).to.equal('1');
+      return support.redisGetAll('impressions.episodes.*');
+    }).then(vals => {
+      expect(Object.keys(vals).length).to.equal(0);
+    });
+  });
+
+  it('expires redis keys', () => {
+    let incr = new LegacyRedisIncrements([
+      record(1490827132, 'download', 1234, 'abcd')
+    ]);
+    return incr.insert().then(result => {
+      expect(result.length).to.equal(1);
+      expect(result[0].dest).to.equal('redis://127.0.0.1');
+      expect(result[0].count).to.equal(2 * 2);
+      return support.redisTTLAll('downloads.*');
+    }).then(ttlMap => {
+      expect(Object.keys(ttlMap).length).to.equal(4);
+      Object.keys(ttlMap).forEach(key => {
+        expect(ttlMap[key]).to.equal(7200);
+      });
+    });
+  });
+
+});
+
+// helpers
+function record(timestamp, type, id, guid, isdup) {
+  return {
+    timestamp: timestamp,
+    type: type,
+    feederPodcast: id,
+    feederEpisode: guid,
+    isDuplicate: !!isdup
+  }
+}
