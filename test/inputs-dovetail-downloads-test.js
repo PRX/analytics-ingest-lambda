3b4164760f2d3bc292f486f4b3d041110ec2ee60
cavis <ryancavis@gmail.com>
2019-02-14 16:20:01 -0700

    More multi table inserts

--- test/inputs-dovetail-downloads-test.js
+++ test/inputs-dovetail-downloads-test.js
@@ -14,6 +14,22 @@ describe('dovetail-downloads', () => {
     expect(download.check({type: 'download'})).to.be.false;
     expect(download.check({type: 'combined', download: null})).to.be.false;
     expect(download.check({type: 'combined', download: {}})).to.be.true;
+    expect(download.check({type: 'postbytes'})).to.be.false;
+    expect(download.check({type: 'postbytes', download: {}})).to.be.true;
+    expect(download.check({type: 'postbytespreview'})).to.be.false;
+    expect(download.check({type: 'postbytespreview', download: {}})).to.be.true;
+  });
+
+  it('knows the table names of records', () => {
+    expect(download.tableName({type: 'combined'})).to.equal('dt_downloads');
+    expect(download.tableName({type: 'postbytes'})).to.equal('dt_downloads');
+    expect(download.tableName({type: 'postbytespreview'})).to.equal('dt_downloads_preview');
+  });
+
+  it('knows which records are bytes', () => {
+    expect(download.isBytes({type: 'combined'})).to.be.false;
+    expect(download.isBytes({type: 'postbytes'})).to.be.true;
+    expect(download.isBytes({type: 'postbytespreview'})).to.be.true;
   });
 
   it('formats table inserts', async () => {
@@ -76,15 +92,29 @@ describe('dovetail-downloads', () => {
       {type: 'impression', requestUuid: 'the-uuid2', timestamp: 1490827132999},
       {type: 'combined', download: {}, listenerEpisode: 'list-ep-3', timestamp: 1490827132999},
       {type: 'combined', download: {}, listenerEpisode: 'list-ep-4', timestamp: 1490827132999},
+      {type: 'postbytespreview', download: {}, listenerEpisode: 'list-ep-5', timestamp: 1490827132999},
+      {type: 'postbytes', download: {}, listenerEpisode: 'list-ep-6', timestamp: 1490827132999}
     ]);
     return download2.insert().then(result => {
-      expect(result.length).to.equal(1);
+      expect(result.length).to.equal(2);
+
       expect(result[0].dest).to.equal('dt_downloads');
-      expect(result[0].count).to.equal(3);
-      expect(inserts['dt_downloads'].length).to.equal(3);
+      expect(result[0].count).to.equal(4);
+      expect(inserts['dt_downloads'].length).to.equal(4);
       expect(inserts['dt_downloads'][0].json.listener_episode).to.equal('list-ep-1');
+      expect(inserts['dt_downloads'][0].json.is_bytes).to.equal(false);
       expect(inserts['dt_downloads'][1].json.listener_episode).to.equal('list-ep-3');
+      expect(inserts['dt_downloads'][1].json.is_bytes).to.equal(false);
       expect(inserts['dt_downloads'][2].json.listener_episode).to.equal('list-ep-4');
+      expect(inserts['dt_downloads'][2].json.is_bytes).to.equal(false);
+      expect(inserts['dt_downloads'][3].json.listener_episode).to.equal('list-ep-6');
+      expect(inserts['dt_downloads'][3].json.is_bytes).to.equal(true);
+
+      expect(result[1].dest).to.equal('dt_downloads_preview');
+      expect(result[1].count).to.equal(1);
+      expect(inserts['dt_downloads_preview'].length).to.equal(1);
+      expect(inserts['dt_downloads_preview'][0].json.listener_episode).to.equal('list-ep-5');
+      expect(inserts['dt_downloads_preview'][0].json.is_bytes).to.equal(true);
     });
   });
 
