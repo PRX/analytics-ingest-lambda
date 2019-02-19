f14279b8646f70b6d2a23caf72f2ed1c31f54fe1
cavis <ryancavis@gmail.com>
2019-02-19 16:29:11 -0700

    Store ante-bytes in DDB

---
+++ test/inputs-ante-bytes-test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+require('./support');
+const dynamo = require('../lib/dynamo');
+const AnteBytes = require('../lib/inputs/ante-bytes');
+
+describe('ante-bytes', () => {
+
+  it('recognizes ante-byte records', () => {
+    const bytes = new AnteBytes();
+    expect(bytes.check({})).to.be.false;
+    expect(bytes.check({type: 'anything'})).to.be.false;
+    expect(bytes.check({type: 'bytes'})).to.be.false;
+    expect(bytes.check({type: 'antebytes'})).to.be.true;
+    expect(bytes.check({type: 'antebytespreview'})).to.be.true;
+  });
+
+  it('formats dynamodb records', () => {
+    const bytes = new AnteBytes();
+    const formatted = bytes.format({
+      listenerSession: 'ls1',
+      digest: 'digest1',
+      something: 'else',
+    });
+    expect(formatted).to.eql({id: 'ls1.digest1', something: 'else'});
+  });
+
+  it('inserts nothing', () => {
+    return new AnteBytes().insert().then(result => {
+      expect(result.length).to.equal(0);
+    });
+  });
+
+  it('inserts ddb records', async () => {
+    sinon.stub(dynamo, 'write').callsFake(recs => Promise.resolve(recs.length));
+
+    const bytes = new AnteBytes([
+      {type: 'combined', listenerSession: 'ls1', digest: 'd1', test: 'one'},
+      {type: 'antebytes', listenerSession: 'ls2', digest: 'd2', test: 'two'},
+      {type: 'antebytespreview', listenerSession: 'ls3', digest: 'd3', test: 'three'},
+      {type: 'postbytes', listenerSession: 'ls4', digest: 'd4', test: 'four'},
+      {type: 'postbytespreview', listenerSession: 'ls5', digest: 'd5', test: 'five'},
+      {type: 'antebytes', listenerSession: 'ls2', digest: 'd2', test: 'duplicate'},
+    ]);
+    expect(await bytes.insert()).to.eql([{dest: 'dynamodb', count: 3}]);
+    expect(dynamo.write).to.have.been.calledOnce;
+
+    const recs = dynamo.write.args[0][0];
+    expect(recs.length).to.equal(3);
+    expect(recs[0]).to.eql({id: 'ls2.d2', type: 'antebytes', test: 'two'});
+    expect(recs[1]).to.eql({id: 'ls3.d3', type: 'antebytespreview', test: 'three'});
+    expect(recs[2]).to.eql({id: 'ls2.d2', type: 'antebytes', test: 'duplicate'});
+  });
+
+});
