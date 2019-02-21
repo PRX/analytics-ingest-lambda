4f833292f22e1dbbb97d856f662f7e31bf606fce
cavis <ryancavis@gmail.com>
2019-02-21 14:02:54 -0700

    Don't need to know the source arn

--- test/support/build.js
+++ test/support/build.js
@@ -4,7 +4,6 @@
 exports.buildRecord = (record) => {
   return {
     eventSource: 'aws:kinesis',
-    eventSourceARN: 'arn:aws:kinesis:us-east-1:12345678:stream/the-stream-name',
     eventVersion: '1.0',
     kinesis: {
       data: new Buffer(JSON.stringify(record), 'utf-8').toString('base64')
