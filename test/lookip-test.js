b8420b02c3a464e01d75c324da31524f0a571e50
cavis <ryancavis@gmail.com>
2019-06-12 16:23:29 -0600

    Allow masking leftmost IP of xff header

--- test/lookip-test.js
+++ test/lookip-test.js
@@ -5,6 +5,31 @@ const lookip = require('../lib/lookip');
 
 describe('lookip', () => {
 
+  it('cleans ips', () => {
+    expect(lookip.clean('')).to.equal(undefined);
+    expect(lookip.clean(',blah')).to.equal(undefined);
+    expect(lookip.clean(',999.999.999.999')).to.equal('999.999.999.999');
+    expect(lookip.clean(', , 66.6.44.4 ,99.99.99.99')).to.equal('66.6.44.4');
+  });
+
+  it('cleans a list of ips', () => {
+    expect(lookip.cleanAll('')).to.equal(undefined);
+    expect(lookip.cleanAll(',blah')).to.equal(undefined);
+    expect(lookip.cleanAll(', , 66.6.44.4 ,99.99.99.99, blah')).to.equal('66.6.44.4, 99.99.99.99');
+  });
+
+  it('masks ips', () => {
+    expect(lookip.mask('blah')).to.equal('blah');
+    expect(lookip.mask('1234.5678.1234.5678')).to.equal('1234.5678.1234.5678');
+    expect(lookip.mask('192.168.0.1')).to.equal('192.168.0.0');
+    expect(lookip.mask('2804:18:1012:6b65:1:3:3561:14b8')).to.equal('2804:18:1012:6b65:1:3:3561:0');
+  });
+
+  it('masks the leftmost x-forwarded-for ip', () => {
+    expect(lookip.maskLeft('66.6.44.4, 99.99.99.99')).to.equal('66.6.44.0, 99.99.99.99');
+    expect(lookip.maskLeft('unknown, 99.99.99.99, 127.0.0.1')).to.equal('unknown, 99.99.99.99, 127.0.0.1');
+  });
+
   it('looks up geo data', () => {
     return lookip.look('66.6.44.4').then(look => {
       expect(look.city).to.equal(5128581);
