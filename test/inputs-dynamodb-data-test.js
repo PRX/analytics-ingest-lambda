85835afe05066caced3c6ef3766524fae0309749
cavis <ryancavis@gmail.com>
2021-06-28 16:32:27 -0600

    Move concurrency test

--- test/inputs-dynamodb-data-test.js
+++ test/inputs-dynamodb-data-test.js
@@ -187,6 +187,62 @@ describe('dynamodb-data', () => {
     });
   });
 
+  describe('updateAll', () => {
+    it('limits concurrent dynamo updates', async () => {
+      const ddb = new DynamodbData();
+      sinon.stub(logger, 'error');
+      sinon.stub(dynamo, 'client').callsFake(async () => 'my-client');
+      sinon.stub(ddb, 'format').callsFake(() => []);
+
+      // stub promises as update is called
+      const promises = [],
+        resolvers = [],
+        rejectors = [];
+      sinon.stub(dynamo, 'update').callsFake(() => {
+        const p = new Promise((res, rej) => {
+          resolvers.push(res);
+          rejectors.push(rej);
+        });
+        promises.push(p);
+        return p;
+      });
+
+      // to start, we should see 5 calls
+      const args = ['my-id', { my: 'data' }, ['my', 'segments']];
+      const updateAllPromise = ddb.updateAll(Array(10).fill(args), 5);
+      await new Promise(r => process.nextTick(r));
+      expect(promises.length).to.equal(5);
+
+      // resolving any picks up new
+      resolvers[0](0);
+      resolvers[3](3);
+      resolvers[4](4);
+      await new Promise(r => process.nextTick(r));
+      expect(promises.length).to.equal(8);
+
+      // as do errors
+      rejectors[1](1);
+      rejectors[5](5);
+      await new Promise(r => process.nextTick(r));
+      expect(promises.length).to.equal(10);
+      expect(logger.error).to.have.callCount(2);
+      expect(logger.error.args[0][0]).to.match(/DDB Error/);
+      expect(logger.error.args[1][0]).to.match(/DDB Error/);
+
+      // finish up
+      resolvers[2](2);
+      resolvers[6](6);
+      resolvers[7](7);
+      resolvers[8](8);
+      resolvers[9](9);
+
+      const [updateCount, failureCount, loggedCount] = await updateAllPromise;
+      expect(updateCount).to.equal(8);
+      expect(failureCount).to.equal(2);
+      expect(loggedCount).to.equal(0);
+    });
+  });
+
   describe('#format', () => {
     const ddb = new DynamodbData();
 
