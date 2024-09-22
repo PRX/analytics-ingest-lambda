2be2d122a86f994bda6756874f94ad772fdabedf
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-09-21 20:24:01 -0400

    Check for freq, use max days for filter and evict

--- test/inputs-dovetail-frequency-test.js
+++ test/inputs-dovetail-frequency-test.js
@@ -6,23 +6,39 @@ const dynamo = require('../lib/dynamo');
 const DovetailFrequency = require('../lib/inputs/dovetail-frequency');
 
 describe('dovetail-frequency', () => {
-  let impression = new DovetailFrequency();
+  let frequency = new DovetailFrequency();
+
+  it('recognizes a frequency impression', () => {
+    expect(frequency.checkFrequency({})).to.be.false;
+    expect(frequency.checkFrequency({frequency: null})).to.be.false;
+    expect(frequency.checkFrequency({frequency: ""})).to.be.false;
+    expect(frequency.checkFrequency({frequency: "foo"})).to.be.false;
+    expect(frequency.checkFrequency({frequency: "1:1"})).to.be.true;
+    expect(frequency.checkFrequency({frequency: "1:1,2:4"})).to.be.true;
+  });
+
+  it('recognizes frequency impressions', () => {
+    expect(frequency.checkImpressions([{},{frequency: null},{frequency: ""},{frequency: "foo"}])).to.be.false;
+    expect(frequency.checkImpressions([{},{frequency: "1:1"},{}])).to.be.true;
+    expect(frequency.checkImpressions([{},{frequency: "1:1,2:4"}])).to.be.true;
+  });
 
   it('recognizes impression records', () => {
-    expect(impression.check({ type: null })).to.be.false;
-    expect(impression.check({ type: undefined })).to.be.false;
-    expect(impression.check({ type: 'download' })).to.be.false;
-    expect(impression.check({ type: 'impression' })).to.be.false;
-    expect(impression.check({ type: 'postbytes', impressions: [] })).to.be.false;
-    expect(impression.check({ type: 'postbytes', impressions: [{}] })).to.be.true;
+    expect(frequency.check({ type: null })).to.be.false;
+    expect(frequency.check({ type: undefined })).to.be.false;
+    expect(frequency.check({ type: 'download' })).to.be.false;
+    expect(frequency.check({ type: 'impression' })).to.be.false;
+    expect(frequency.check({ type: 'postbytes', impressions: [] })).to.be.false;
+    expect(frequency.check({ type: 'postbytes', impressions: [{}] })).to.be.false;
+    expect(frequency.check({ type: 'postbytes', impressions: [{frequency: "1:1"}] })).to.be.true;
   });
 
   it('knows the table names of records', () => {
-    expect(impression.tableName()).to.equal('DovetailListenerFrequency');
+    expect(frequency.tableName()).to.equal('listener_frequency_dev_local');
   });
 
   it('inserts nothing', () => {
-    return impression.insert().then(result => {
+    return frequency.insert().then(result => {
       expect(result.length).to.equal(0);
     });
   });
@@ -40,11 +56,12 @@ describe('dovetail-frequency', () => {
     } else {
       sinon.stub(dynamo, 'client').callsFake(async () => 'my-client');
       sinon.stub(dynamo, 'updateItemPromise').callsFake(async params => {
+        const current = Math.floor((new Date().getTime() / 1000));
         let exp = '';
-        let imp = [];
+        let imp = [`${current - 2600000}`, `${current - 2700000}`, `${current - 2800000}`, `${current - 2900000}`, `${current - 3000000}`, `${current - 3100000}`, `${current - 3200000}`, `${current - 3300000}`, `${current - 3400000}`, `${current - 3500000}`, `${current - 3600000}`];
         if (params.ExpressionAttributeValues[":ttl"]) {
           exp = params.ExpressionAttributeValues[":ttl"].N;
-          imp = params.ExpressionAttributeValues[":ts"].NS;
+          imp = imp.concat(params.ExpressionAttributeValues[":ts"].NS);
         }
         return {
           Attributes: {
@@ -72,7 +89,7 @@ describe('dovetail-frequency', () => {
         type: 'postbytes',
         listenerId: 'listener2',
         timestamp: `${current - 90}`,
-        impressions: [{ campaignId: 100 }, { campaignId: 200 }],
+        impressions: [{ campaignId: 100, frequency: "1:1" }, { campaignId: 200, frequency: "1:1" }],
       },
       {
         type: 'postbytes',
@@ -84,37 +101,37 @@ describe('dovetail-frequency', () => {
         type: 'postbytes',
         listenerId: 'listener2',
         timestamp: `${current - 2600000}`,
-        impressions: [{ campaignId: 400 }],
+        impressions: [{ campaignId: 400, frequency: "1:1" }],
       },
       {
         type: 'postbytes',
         listenerId: 'listener4',
         timestamp: `${current - 60}`,
-        impressions: [{ campaignId: 500 }],
+        impressions: [{ campaignId: 500, frequency: "1:1" }],
       },
       {
         type: 'postbytes',
         listenerId: 'listener4',
         timestamp: `${current - 50}`,
-        impressions: [{ campaignId: 500 }],
+        impressions: [{ campaignId: 500, frequency: "1:1" }],
       },
       {
         type: 'postbytes',
         listenerId: 'listener4',
         timestamp: `${current - 50}`,
-        impressions: [{ campaignId: 500 }],
+        impressions: [{ campaignId: 500, frequency: "1:1" }],
       },
       {
         type: 'postbytes',
         listenerId: 'listener2',
         timestamp: `${current - 40}`,
-        impressions: [{ campaignId: 400 }],
+        impressions: [{ campaignId: 400, frequency: "1:1" }],
       },
     ]);
     return frequency.insert().then(result => {
       expect(result).to.eql([
-        { count: 7, dest: 'updates' },
-        { count: 1, dest: 'deletes' },
+        { count: 6, dest: 'updates' },
+        { count: 6, dest: 'deletes' },
       ]);
     });
   });
